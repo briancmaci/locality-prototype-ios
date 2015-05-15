27b7024d689ae4fb02ce21937e73a00d2bd1f154
Brian Maci <brian.maci@mry.com>
2015-05-15 12:30:14 -0400

    Range creation and map animation

---
+++ locality/util/AppUtilities.h
@@ -0,0 +1,13 @@
+//
+//  AppUtilities.h
+//  locality
+//
+//  Created by Brian Maci on 5/15/15.
+//  Copyright (c) 2015 briancmaci. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppUtilities : NSObject
+
+@end
---
+++ locality/util/AppUtilities.m
@@ -0,0 +1,13 @@
+//
+//  AppUtilities.m
+//  locality
+//
+//  Created by Brian Maci on 5/15/15.
+//  Copyright (c) 2015 briancmaci. All rights reserved.
+//
+
+#import "AppUtilities.h"
+
+@implementation AppUtilities
+
+@end
