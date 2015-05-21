a733abdd1d7757edfa4e5a9c7aef4998a5db5e41
Brian Maci <brian.maci@mry.com>
2015-05-21 15:08:08 -0400

    Post Button addition, Busy View creation, fonts install, settings button fix

---
+++ locality/custom_ui/BusyView.h
@@ -0,0 +1,13 @@
+//
+//  BusyView.h
+//  locality
+//
+//  Created by Brian Maci on 5/21/15.
+//  Copyright (c) 2015 briancmaci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BusyView : UIView
+
+@end
---
+++ locality/custom_ui/BusyView.m
@@ -0,0 +1,21 @@
+//
+//  BusyView.m
+//  locality
+//
+//  Created by Brian Maci on 5/21/15.
+//  Copyright (c) 2015 briancmaci. All rights reserved.
+//
+
+#import "BusyView.h"
+
+@implementation BusyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
